80702d8bda9e7ed8dee215da700607b1d9188729
Matias Storm <matias.storm42@gmail.com>
2022-04-21 20:59:20 +0200

    Add eslint

--- GAT-desktop/src/main.js
+++ GAT-desktop/src/main.js
@@ -1,16 +1,15 @@
 const path = require("path");
-const { app, BrowserWindow } = require( 'electron');
+const { app, BrowserWindow } = require("electron");
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
     });
 
     win.loadFile(path.join(__dirname, "../dist/index.html"));
 };
 
 app.whenReady().then(() => {
-    createWindow()
+    createWindow();
 });
-
---
+++ GAT-desktop/src/web/index.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+function App() {
+    return <h1>This is my React app!</h1>;
+}
+
+const container = document.getElementById("app");
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
