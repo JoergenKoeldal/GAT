6f7bb1653b201e3d84ab1cbe4bc72ca47eb62e89
Matias Storm <matias.storm42@gmail.com>
2022-04-21 12:04:34 +0200

    Use react 18's createRoot api

--- GAT-desktop/src/web/index.js
+++ GAT-desktop/src/web/index.js
@@ -1,6 +1,11 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import React from 'react';
 const App = () => {
     return <h1>This is my React app!</h1>;
 }
-ReactDOM.render(<App />, document.getElementById('app'));
+
+const container = document.getElementById('app');
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
