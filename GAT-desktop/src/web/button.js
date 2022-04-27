1f4a4d2489ac7d8a0c8cda626ee9dd2cd7a0f77c
Phillip Vendel Glimø <phillip.glimoe@hotmail.com>
2022-04-27 13:17:02 +0200

    Rename index.jsx to index.js

---
+++ GAT-desktop/src/web/button.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Button() {
+    return <button>Click me!</button>;
+}
+
--- GAT-desktop/src/web/index.js
+++ GAT-desktop/src/web/index.js
@@ -3,6 +3,7 @@ import React from "react";
 
 function App() {
     return <h1>This is my React app!</h1>;
+
 }
 
 const container = document.getElementById("app");
