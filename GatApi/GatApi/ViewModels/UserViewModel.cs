6a124f2e8c095d55ad1e1302a37f156f5c92affc
PVGlimoe <phillip.glimoe@hotmail.com>
2022-05-01 12:38:05 +0200

    create userViewModel

---
+++ GatApi/GatApi/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GatApi.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsScheduleFinished { get; set; }
+        public string DepartmentName { get; set; }
+
+    }
+}
