c44ad30f9009a23335945bdaff2bee70e3124285
PVGlimoe <phillip.glimoe@hotmail.com>
2022-05-08 23:49:46 +0200

    Create sourcesController

---
+++ GatApi/GatApi/Controllers/SourcesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GatApi.Data;
+using GatApi.Models;
+
+namespace GatApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SourcesController : ControllerBase
+    {
+        private readonly GatApiContext _context;
+
+        public SourcesController(GatApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Sources
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Source>>> GetSource()
+        {
+            return await _context.Source.ToListAsync();
+        }
+
+        // GET: api/Sources/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Source>> GetSource(long id)
+        {
+            var source = await _context.Source.FindAsync(id);
+
+            if (source == null)
+            {
+                return NotFound();
+            }
+
+            return source;
+        }
+
+        // PUT: api/Sources/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSource(long id, Source source)
+        {
+            if (id != source.SourceId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(source).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SourceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Sources
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Source>> PostSource(Source source)
+        {
+            _context.Source.Add(source);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSource", new { id = source.SourceId }, source);
+        }
+
+        // DELETE: api/Sources/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSource(long id)
+        {
+            var source = await _context.Source.FindAsync(id);
+            if (source == null)
+            {
+                return NotFound();
+            }
+
+            _context.Source.Remove(source);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SourceExists(long id)
+        {
+            return _context.Source.Any(e => e.SourceId == id);
+        }
+    }
+}
