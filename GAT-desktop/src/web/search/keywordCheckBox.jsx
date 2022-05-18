2e2183cd54977c132fd2407aa6f734916b56d7c8
Joergen Koeldal <joer1894@stud.kea.dk>
2022-05-18 15:28:35 +0200

    add state to checkboxes

--- GAT-desktop/src/web/search/keywordCheckBox.jsx
+++ GAT-desktop/src/web/search/keywordCheckBox.jsx
@@ -9,6 +9,7 @@ import Collapsible from "../util/collapsible";
 export default function KeywordListCheckBox() {
 
     const [keywordList, setKeywordList] = useState([])
+    const [checkBoxChecked, setcheckBoxState] = useState([]);
 
     useEffect(() => {
         const fetchkeywordList = async () => {
@@ -23,7 +24,7 @@ export default function KeywordListCheckBox() {
         checkboxes.push(
             <div key={e.name}>
             <label key={e.name} >
-                <input type="checkbox" value={e.name} />
+                    <input type="checkbox" value={e.name} checked={checkBoxChecked.includes(e.name)} onChange={ checkBoxState} />
                 </label>
                 <Collapsible buttonTitle={e.name}>
                     
@@ -38,6 +39,20 @@ export default function KeywordListCheckBox() {
                 </div>
         )
     })
+
+    function checkBoxState(evt) {
+
+        if (evt.target.checked) {
+            //Tager checkbox staten, pakker alle elementerne ud (de 3 punktummer), og til sidst sættes det sidste element på
+            setcheckBoxState([...checkBoxChecked, evt.target.value])
+        } else {
+            //Filtrere den ikke markerede listes navne fra
+            setcheckBoxState(checkBoxChecked.filter((c) => c != evt.target.value))
+        }
+       
+    }
+
+
 
 
     return (
--- GAT-desktop/src/web/search/sourceCheckBox.jsx
+++ GAT-desktop/src/web/search/sourceCheckBox.jsx
@@ -9,6 +9,7 @@ import { getSources } from "../apiService";
 export default function SourceCheckBox() {
 
     const [sources, setSources] = useState([])
+    const [checkBoxChecked, setcheckBoxState] = useState([]);
 
     useEffect(() =>  {
         const fetchSources = async () => {
@@ -23,9 +24,25 @@ export default function SourceCheckBox() {
         checkboxes.push(
             <label key={e.sourceId}>
                 {e.name}
-            <input type="checkbox" value={e.sourceId} />
+                <input type="checkbox" value={e.name} checked={checkBoxChecked.includes(e.name + "")} onChange={checkBoxState} />
         </label>)
-	})
+    })
+
+
+    function checkBoxState(evt) {
+
+        if (evt.target.checked) {
+            //Tager checkbox staten, pakker alle elementerne ud (de 3 punktummer), og til sidst sættes det sidste element på
+            setcheckBoxState([...checkBoxChecked, evt.target.value])
+        } else {
+            //Filtrere den ikke markerede listes navne fra
+            setcheckBoxState(checkBoxChecked.filter((c) => c != evt.target.value))
+        }
+
+    }
+
+    console.log(checkBoxChecked);
+
 
     return (
         <div>
